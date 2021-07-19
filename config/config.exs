c893a100ff3a57ed0b9ccf81f675d3fa89432acd
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-07-19 14:31:47 +0600

    feat: finely tweaked the config that works

--- config/config.exs
+++ config/config.exs
@@ -3,19 +3,19 @@ import Config
 config :bamboo_gmail_smtp, BambooGmailSmtp.Mailer,
   adapter: Bamboo.SMTPAdapter,
   server: System.get_env("GMAIL_SMTP_URL"),
-  hostname: System.get_env("SMTP_HOST_NAME"),
+  hostname: System.get_env("SMTP_HOST_NAME"), # You can comment this line
   port: System.get_env("GMAIL_SMTP_PORT"),
-  username: System.get_env("GMAIL_SMTP_USER"), # or {:system, "SMTP_USERNAME"}
-  password: System.get_env("GMAIL_SMTP_PASSWORD"), # or {:system, "SMTP_PASSWORD"}
+  username: System.get_env("GMAIL_SMTP_USER"),
+  password: System.get_env("GMAIL_SMTP_PASSWORD"),
   tls: :always, # can be `:always` or `:never`
-  allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"], # or {:system, "ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
+  allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"],
   tls_log_level: :error,
   # tls_verify: :verify_peer, # optional, can be `:verify_peer` or `:verify_none`
   # tls_cacertfile: "/somewhere/on/disk", # optional, path to the ca truststore
   # tls_cacerts: "…", # optional, DER-encoded trusted certificates
   # tls_depth: 3, # optional, tls certificate chain depth
   # tls_verify_fun: {&:ssl_verify_hostname.verify_fun/3, check_hostname: "example.com"}, # optional, tls verification function
-  ssl: true, # can be `true`
+  ssl: false, # can be `true`, you'll face some ugly error if you se this to true
   retries: 1,
   no_mx_lookups: false, # can be `true`
-  auth: :if_available # can be `:always`. If your smtp relay requires authentication set it to `:always`.
+  auth: :always # can be `:always`. If your smtp relay requires authentication set it to `:always`.
