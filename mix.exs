ea0ccecdd2d286b30d627b3a3e6c89d35515f32b
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-07-19 12:22:18 +0600

    feat: bamboo added to application start list

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule BambooGmailSmtp.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      applications: [:bamboo, :bamboo_smtp]
     ]
   end
 
