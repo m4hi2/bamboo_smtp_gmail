e548328e719e96de2e0a7d91adb5f6c66c903826
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-07-19 14:32:52 +0600

    feat: email structure

--- lib/bamboo_gmail_smtp.ex
+++ lib/bamboo_gmail_smtp.ex
@@ -3,10 +3,15 @@ defmodule BambooGmailSmtp do
   Documentation for `BambooGmailSmtp`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
+  def welcome_email do
+    new_email(
+      to: "test@example.com",
+      from: System.get_env("GMAIL_SMTP_USER"), # Gmail doesn't let you change it.
+      subject: "Welcome to the app.",
+      html_body: "<strong>Thanks for joining!</strong>",
+      text_body: "Thanks for joining!"
+    )
+  end
 
       iex> BambooGmailSmtp.hello()
       :world
